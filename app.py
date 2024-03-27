d5e6806613999e4c78afafc5b787420a8a88a28c
Trying2Code <89550200+Trying2Code@users.noreply.github.com>
2024-03-27 18:08:42 +0000

    added routes to pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, redirect, url_for, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -39,10 +39,22 @@ JOBS = [
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='FuturRelevance Connect')
 
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+
+@app.route("/frequently")
+def frequently_page():
+    return render_template('frequently.html')
+
 @app.route("/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+
+
+  
+
   
