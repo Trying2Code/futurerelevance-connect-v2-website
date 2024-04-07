f9cb66eb727c2a078ee078145886b9f89158bbd2
Trying2Code <89550200+Trying2Code@users.noreply.github.com>
2024-04-07 21:42:41 +0000

    added new templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def hello_FutureRelevavce():
   jobs = load_jobs_from_db()
   return render_template('home.html',
                          jobs=jobs,
-                         company_name='FuturRelevanc Connect')
+                         company_name='FuturRelevance Connect')
 
 @app.route("/login")
 def login_page():
@@ -30,6 +30,13 @@ def frequently_page():
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html', job=job)    
     
 
 if __name__ == '__main__':
