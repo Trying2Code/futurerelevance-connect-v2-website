bc89ef728e093f46ab0aa456d843537da2e18f95
Trying2Code <89550200+Trying2Code@users.noreply.github.com>
2024-04-09 11:22:52 +0000

    form submission to database

--- database.py
+++ database.py
@@ -11,6 +11,7 @@ engine  = create_engine(
       "ssl_ca": "/etc/ssl/cert.pem"
     }
   })
+
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
@@ -21,12 +22,21 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs where id = :val"), {"val":id})
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),  {"val": id})
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
       return rows[0]._mapping
-      
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn: 
+    query = text("INSERT INTO applications (job_id, first_name, last_name, email, street_address, unit_suburb, city, country, area_code, education, work_experience, resume_url) values (:job_id, :first_name, :last_name, :email, :street_address, :unit_suburb, :city,  :country,  :area_code, :education, :work_experience, :resume_url)")
+    
+    conn.execute(query, {"job_id": job_id, "first_name": data['first_name'], "last_name": data['last_name'], "email": data['email'], "street_address" : data['street_address'], "unit_suburb" : data['unit_suburb'], "city" : data['city'], "country" : data['country'], "area_code" : data['area_code'] , "education" : data['education'], "work_experience" : data['work_experience'], "resume_url" : data['resume_url']})
+    
+                 
+
       
       
